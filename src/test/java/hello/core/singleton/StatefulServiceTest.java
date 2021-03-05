f7b2ce44bd452a48a98deaeeee0f3dd80145a064
TaeHyeon Kim <kth990303@naver.com>
2021-03-06 00:53:19 +0900

    싱글톤 주의점

--- src/test/java/hello/core/singleton/StatefulServiceTest.java
+++ src/test/java/hello/core/singleton/StatefulServiceTest.java
@@ -18,6 +18,7 @@ public class StatefulServiceTest {
         int price2 = s2.order("userB", 20000);
         // 지역변수는 공유되는게 아니니까
         // 싱글톤 방식으로 할 경우, 클래스 변수 및 공유값은 건드리지 않도록
+        // stateless한 객체로
         Assertions.assertThat(price1).isNotEqualTo(price2);
     }
     static class TestConfig{
