13437c2afd37de6b5f3db4f8dbcf6ff1f220da4c
TaeHyeon Kim <kth990303@naver.com>
2021-03-16 23:17:55 +0900

    componentscan 설명 추가

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -6,8 +6,9 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
+//        basePackages = "hello.core",
         excludeFilters = @ComponentScan.Filter(type= FilterType.ANNOTATION, classes=Configuration.class)
 )
 public class AutoAppConfig {
-
+    // 패키지 위치 지정 x, 설정 정보 클래스 위치를 프로젝트 최상단에
 }
