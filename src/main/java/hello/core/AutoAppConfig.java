d209aef43f0afbe56b231c9b572f1336f368e1ba
TaeHyeon Kim <kth990303@naver.com>
2021-03-23 19:53:07 +0900

    lombok을 적용하고 @primary 적용. @qualifier은 따로 클래스 만들음

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -1,5 +1,8 @@
 package hello.core;
 
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -7,8 +10,9 @@ import org.springframework.context.annotation.FilterType;
 @Configuration
 @ComponentScan(
 //        basePackages = "hello.core",
-        excludeFilters = @ComponentScan.Filter(type= FilterType.ANNOTATION, classes=Configuration.class)
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
     // 패키지 위치 지정 x, 설정 정보 클래스 위치를 프로젝트 최상단에
+
 }
---
+++ src/main/java/hello/core/annotation/MainDiscountPolicy.java
@@ -0,0 +1,18 @@
+package hello.core.annotation;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+
+import java.lang.annotation.*;
+
+// qualifierd 클래스에서 가져온 어노테이션
+@Target({ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER, ElementType.TYPE, ElementType.ANNOTATION_TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Inherited
+@Documented
+// 이렇게 클래스를 만들면
+// 오타로 인한 잘못 지정된 qualifier어노테이션
+// 컴파일 에러로 잡아낼 수 있음
+@Qualifier("mainDiscountPolicy")
+public @interface MainDiscountPolicy {
+
+}
